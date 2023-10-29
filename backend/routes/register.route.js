dc2e76fa0ab8d651da7498803ea8e00e7afacd02
Hopp-Murithi <hoppmurithi@gmail.com>
2023-10-29 18:06:32 +0300

    configure route to user reigstration

--- backend/routes/register.route.js
+++ backend/routes/register.route.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const { check } = require("express-validator");
 
 const { registerUser } = require("../controllers/auth/register.controller");
+const {
+  payloadValidationErrors,
+} = require("../helpers/payloadValidationErrors.helper");
+const { authValidations } = require("../validations/auth.validations");
 
 const auth = express.Router();
 
-/**
- * configure route path
- */
 const authBaseUrl = "/auth/user";
-
-/**
- * create post route to register user and validate there is input
- */
 
 auth.post(
   authBaseUrl + "/register",
-  registerUser
+  authValidations["registration"],
+  payloadValidationErrors,
+  registerUser,
 );
 
 module.exports = { auth };
