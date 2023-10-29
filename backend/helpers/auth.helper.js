865d3ad4284375e067950400edb7797a9c2e0988
Hopp-Murithi <hoppmurithi@gmail.com>
2023-10-29 18:04:09 +0300

    add authentication helper

---
+++ backend/helpers/auth.helper.js
@@ -0,0 +1,19 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const hashPassword = (rawPassword) => {
+    return bcrypt.hashSync(rawPassword, Number(process.env.PASSWORD_SALT_ROUNDS));
+  };
+
+  const createAuthCookie = (res, cookiePayload) => {
+    const cookie = jwt.sign(cookiePayload, process.env.JWT_SECRET, {
+      expiresIn: process.env.COOKIE_EXPIRES_TIME,
+    });
+  
+    res.cookie("cookie", cookie, {
+      expires: new Date(Date.now() + Number(10) * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+    });
+  };
+  
+  module.exports = { hashPassword, createAuthCookie };
