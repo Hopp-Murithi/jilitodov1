1dc12ac193b427ac864fd4346f947400f9a4e7c9
Hopp-Murithi <hoppmurithi@gmail.com>
2023-10-29 18:05:01 +0300

    create middleware to utilize error class

---
+++ backend/middlewares/error_middleware.js
@@ -0,0 +1,39 @@
+const ErrorHandler = require("../helpers/ErrorHandler.helper");
+
+const error_middleware = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  if (String(process.env.NODE_ENV) === "dev") {
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
+
+  if (String(process.env.NODE_ENV) === "prod") {
+    let error = { ...err };
+
+    error.message = err.message;
+
+    /**
+     * wrong JWT error
+     */
+    if (err.name === "JsonWebTokenError") {
+      error = new ErrorHandler("Token is invalid. Try Again", 400);
+    }
+
+    /**
+     * Expired token error
+     */
+    if (err.name === "TokenExpiredError") {
+      error = new ErrorHandler("Token is expired. Kindly login", 400);
+    }
+    res.status(err.statusCode).json({
+      success: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
+
+module.exports = { error_middleware };
