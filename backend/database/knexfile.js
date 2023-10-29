6e146e2331f7623f09c9c787d3f5fc2f2548aa7d
Hopp-Murithi <hoppmurithi@gmail.com>
2023-10-29 18:01:09 +0300

    add database server variable

--- backend/database/knexfile.js
+++ backend/database/knexfile.js
@@ -2,7 +2,8 @@ require("dotenv").config({ path: "../.env" });
 module.exports = {
   client: "pg",
   connection: {
-    host: "localhost", //Make sure its set to localhost when doing migrations
+    host: process.env.DATABASE_SERVER,
+    //host: "localhost", //Make sure its set to localhost when doing migrations
     user: process.env.DATABASE_USER,
     port: Number(process.env.DATABASE_PORT),
     password: process.env.DATABASE_PASSWORD,
