f71353bb09235fb489f2bca7bbc7d417063ec1e3
Hopp-Murithi <hoppmurithi@gmail.com>
2023-11-05 12:05:54 +0300

    refactor to use response helper

--- backend/controllers/health.controller.js
+++ backend/controllers/health.controller.js
@@ -1,22 +1,17 @@
-
+const {CatchAsyncErrors}=require("../helpers/catchAsyncErrors.helper");
+const ErrorHandler=require("../helpers/ErrorHandler.helper");
+const {responseHelper}=require("../helpers/responseHelper");
 /**
  * 
  * @param {*} req request body object
  * @param {*} res response body object
  * @returns true if healthy, false if errored
  */
-const checkHealth = async (req, res) => {
-    try {
-      return res.status(200).json({
-        success: true,
-        message: "Jilitodo api is healthy",
-      });
-    } catch (error) {
-      res.status(500).json({
-        success: false,
-        message: "Internal server error",
-      });
-    }
-  };
+const checkHealth =CatchAsyncErrors(async (req, res, next) => {
+    if (req.errored)
+        return next(new ErrorHandler("Jilitodo api is running into problems"), 404);
+
+    return responseHelper(res, 200, true, "Jilitodo api is healthy");
+});
   
   module.exports = { checkHealth };
